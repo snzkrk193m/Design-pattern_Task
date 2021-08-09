c0b04475b07e7f8d0df0b5f9bc78e0ca3d14d0ee
maron <54880945+snzkrk193m@users.noreply.github.com>
2021-08-10 08:08:18 +0900

    Add files via upload

---
+++ StateTest/TanoshiiState.java
@@ -0,0 +1,22 @@
+class TanoshiiState extends DogState {
+	private static TanoshiiState s = new TanoshiiState();
+
+	private TanoshiiState() {
+	}
+
+	public static DogState getInstance() {
+		return s;
+	}
+
+	public void tukareta(Dog moto) {
+		moto.changeState(IrairaState.getInstance());
+	}
+
+	public void tabeta(Dog moto) {
+		moto.changeState(NeteiruState.getInstance());
+	}
+
+	public String toString() {
+		return "楽しい状態";
+	}
+}
